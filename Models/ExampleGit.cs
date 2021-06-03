471a6a3fb9f100e8c494a487c424b26fe7f5d6ad
Sai Prakash Madala <saiprakashmadala@hotmail.com>
2021-06-03 15:24:45 +0530

    classadd

---
+++ Models/ExampleGit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebapiwithEFDemo.Models
+{
+    public class ExampleGit
+    {
+        public void get()
+        {
+
+        }
+    }
+}
